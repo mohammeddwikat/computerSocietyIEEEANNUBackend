9ea9b7d205d8b59c644818b02056328f799a17f3
Mohammad Dwikat <mohammed.dwikate1999@gmail.com>
2020-08-19 17:21:40 +0300

    test response and reques from react

--- test.php
+++ test.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "HI IEEE family";
- 
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+$signInStatus = new stdClass(); // init json response
+
+$signInStatus-> EmptyField = 'Hi IEEE';
+echo json_encode($signInStatus);
+
 ?>
