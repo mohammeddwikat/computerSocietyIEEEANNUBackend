5a236b659c1a0ad3c83efb4303301fca00f289cb
WaleedTiti99 <waleed.titi8@gmail.com>
2020-08-21 18:34:47 +0300

    FAMILY

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 $signInStatus = new stdClass(); // init json response
 
-$signInStatus-> EmptyField = 'Hi IEEE';
+$signInStatus-> EmptyField = 'Hi IEEE Family';
 echo json_encode($signInStatus);
 
 ?>
